dc71b4421db13e3c8735ff817de0c51a0b74d939
Johannes Breidenbach <92515741+johannesbreidenbach@users.noreply.github.com>
2026-05-30 13:20:53 +0200

    create function to save different pandapower grids with estimation and power flow calculation. First initialization for evaluation.

--- pandapower/test/estimation/af_wlav.py
+++ pandapower/test/estimation/af_wlav.py
@@ -8,6 +8,7 @@ import os
 
 # imports from pandapower
 import pandapower.networks as pn
+from pandapower import to_json
 from pandapower.run import runpp
 from pandapower.estimation import estimate
 from pandapower.create import (create_measurement, create_empty_network, create_bus, create_ext_grid,
@@ -65,7 +66,6 @@ def _add_measurements_af(
 
 def _create_measurement_18_bus_grid(
         net: pandapowerNet,
-        seed: int | None = None,
         rv: float = .01,
         rp: float = .03,
         rq: float = .03
@@ -75,14 +75,12 @@ def _create_measurement_18_bus_grid(
 
     Parameters:
         net: power grid
-        seed:
-            Optional random seed for reproducible simulations. If ``None``, random values are generated for every call.
         rv: standard deviation to apply a multiplicative perturbation to quantities for voltage
         rp: standard deviation to apply a multiplicative perturbation to quantities for active power
         rq: standard deviation to apply a multiplicative perturbation to quantities for reactive power
     """
-    if seed is not None:
-        np.random.seed(seed)  # Set deterministic random numbers for reproducible simulations
+    # if seed is not None:
+    #    np.random.seed(seed)  # Set deterministic random numbers for reproducible simulations
     # =========================================================================
     # Bus 1: v, p, q (index: 0)
     # =========================================================================
@@ -201,8 +199,7 @@ def _create_measurement_18_bus_grid(
 def _create_18_bus_grid(
         base_mva: float = 10.0,
         slack_v: float = 1.0,
-        slack_va_degree: float = 0.0,
-        seed: int | None = None
+        slack_va_degree: float = 0.0
 ) -> tuple[pandapowerNet, dict[str, np.ndarray]]:
     """
     Create the 18-bus radial distribution network from the original MATLAB implementation and run a power flow
@@ -224,9 +221,6 @@ def _create_18_bus_grid(
 
         slack_va_degree: Voltage angle of the slack bus in degrees.
 
-        seed:
-            Optional random seed for reproducible simulations. If ``None``, random values are generated for every call.
-
     Returns:
         Return a pandapower net with the results from the powerflow calculation and the nominal loads and powers for
         state estimation. The dict includes the random scaling factors:
@@ -240,8 +234,8 @@ def _create_18_bus_grid(
                 - ``KG_wind``: wind generation scaling
     """
 
-    if seed is not None:
-        np.random.seed(seed)  # Set deterministic random numbers for reproducible simulations
+    # if seed is not None:
+    #     np.random.seed(seed)  # Set deterministic random numbers for reproducible simulations
 
     # =========================================================================
     # Base values
@@ -532,133 +526,208 @@ def _create_18_bus_grid(
     return net_se, k_dc
 
 
-def calc_different_se(net_base: pandapowerNet, init="flat" , save_path: str = ".") -> None:
+def calc_different_se(net_base: pandapowerNet, failures: list, num_it: str, save_path: str = ".") -> None:
     """
-       Run three different state estimations (AF-WLS, AF-WLAV, AF-LAV) on a specific power grid and return bus voltages,
-       angles, deviations and the allocation factors. The power grid is unobservable.
+    Run three different state estimations (AF-WLS, AF-WLAV, AF-LAV) on a specific power grid and return bus voltages,
+    angles, deviations and the allocation factors. The power grid is unobservable.
 
-       The power grid is solved once with a power flow, measurements are created. The power grid is copied for each
-       estimation algorithm. Different types of results from power flow and estimation are collected in different
-       DataFrames, which will save.
+    The power grid is solved once with a power flow, measurements are created. The power grid is copied for each
+    estimation algorithm. Different types of results from power flow and estimation are collected in different
+    DataFrames, which will save.
 
-       Parameters:
-           net_base: A pandapower grid, where the different state estimation solver will be used.
-           init: Initialization method for the state estimation (e.g. ``"flat"``).
-           save_path: path where the resolution dataframes will save.
+    Parameters:
+        net_base: A pandapower grid, where the different state estimation solver will be used.
+        num_it: String which contains the number of iterations.
+        save_path: path where the resolution dataframes will save.
+        failures: Save all estimation, which does not calc a state estimation (does not found solution).
 
-       Returns:
-           None
-       """
-
-
-    # 3. create copies for each estimation algorithm
-    net_af_wls = copy.deepcopy(net_base)
-    net_af_lav = copy.deepcopy(net_base)
-    net_af_wlav = copy.deepcopy(net_base)
-
-
-    # 4. run estimations with soft-fail (collect errors, do not raise immediately)
-    failures = []
+    Returns:
+        None
+    """
+    # 1. create copies for each estimation algorithm
+    if failures is None:
+        failures = []
 
     # AF-WLS
-    af_wls = estimate(net_af_wls, algorithm="af-wls", wlav=False)  # , af_target_value=.4, af_std_value=.15
-    if not af_wls["success"]:
-        failures.append("AF-WLS estimation failed")
-        v_af_wls = np.full_like(net_base.res_bus.vm_pu.values, np.nan, dtype=float)
-        delta_af_wls = np.full_like(net_base.res_bus.va_degree.values, np.nan, dtype=float)
+    af_wls_file = os.path.join(save_path, f"af_wls_{num_it}.json")
+    if os.path.exists(af_wls_file):
+        print(f"file {af_wls_file} already exists")
     else:
-        v_af_wls = net_af_wls.res_bus_est.vm_pu.values
-        delta_af_wls = net_af_wls.res_bus_est.va_degree.values
+        net_af_wls = copy.deepcopy(net_base)
+        af_wls = estimate(net_af_wls, algorithm="af-wls", wlav=False)  # , af_target_value=.4, af_std_value=.15
+        af_wls["allocation_factors"].index = ["AF-WLS"]
+        if not af_wls["success"]:
+            failures.append(f"AF-WLS, {num_it}, failed")
+        to_json(net_af_wls, af_wls_file)
 
     # LAV
-    af_lav = estimate(net_af_lav, algorithm="af-lp", wlav=False, with_ortools=False)
-    if not af_lav["success"]:
-        failures.append("LAV estimation failed")
-        v_af_lav = np.full_like(net_base.res_bus.vm_pu.values, np.nan, dtype=float)
-        delta_af_lav = np.full_like(net_base.res_bus.va_degree.values, np.nan, dtype=float)
+    af_lav_file = os.path.join(save_path, f"af_lav_{num_it}.json")
+    if os.path.exists(af_lav_file):
+        print(f"file {af_lav_file} already exists")
     else:
-        v_af_lav = net_af_lav.res_bus_est.vm_pu.values
-        delta_af_lav = net_af_lav.res_bus_est.va_degree.values
+        net_af_lav = copy.deepcopy(net_base)
+        af_lav = estimate(net_af_lav, algorithm="af-lp", wlav=False, with_ortools=False)
+        af_lav["allocation_factors"].index = ["AF-LAV"]
+        if not af_lav["success"]:
+            failures.append(f"AF-LAV, {num_it}, failed")
+        to_json(net_af_lav, af_lav_file)
 
     # AF-WLAV
-    af_wlav = estimate(net_af_wlav, algorithm="af-lp", wlav=True, with_ortools=False)
-    if not af_wlav["success"]:
-        failures.append("AF-WLAV estimation failed")
-        v_af_wlav = np.full_like(net_base.res_bus.vm_pu.values, np.nan, dtype=float)
-        delta_af_wlav = np.full_like(net_base.res_bus.va_degree.values, np.nan, dtype=float)
+    af_wlav_file = os.path.join(save_path, f"af_wlav_{num_it}.json")
+    if os.path.exists(af_wlav_file):
+        print(f"file {af_wlav_file} already exists")
     else:
-        v_af_wlav = net_af_wlav.res_bus_est.vm_pu.values
-        delta_af_wlav = net_af_wlav.res_bus_est.va_degree.values
+        net_af_wlav = copy.deepcopy(net_base)
+        af_wlav = estimate(net_af_wlav, algorithm="af-lp", wlav=True, with_ortools=False)
+        af_wlav["allocation_factors"].index = ["AF-WLAV"]
+        if not af_wlav["success"]:
+            failures.append(f"AF-WLAV, {num_it}, failed")
+        to_json(net_af_wlav, af_wlav_file)
 
+    res_af_file = os.path.join(save_path, f"af_df_{num_it}.csv")
+    if os.path.exists(res_af_file):
+        print(f"file {res_af_file} already exists")
+    else:
+        res_af_df = pd.concat([af_wls["allocation_factors"], af_wlav["allocation_factors"], af_lav["allocation_factors"]])
+        res_af_df.to_csv(res_af_file, index=True)
+
+
+def create_random_grid_random_estimation(path: str = ".", itr: int = 1000, seed: int = 112) -> None:
+    """
+    Every iteration will compute random value for load and gen for the powerflow calculation. In addition, also for the
+    measurement for state estimation.
+
+    Parameters:
+        path: Place where grid and allocation factors will be saved.
+        itr: Number of iterations.
+        seed: Optional random seed for reproducible simulations.If ``None``, random values are generated for every call.
+
+    Returns: None
+    """
+    # seed:
+    np.random.seed(112)
+    failures: list = []
+    for i in range(itr):
+        name_str = f"{i:03d}"
+        net18, k = _create_18_bus_grid()
+        _create_measurement_18_bus_grid(net=net18)  # , rv=0, rp=0, rq=0
+        calc_different_se(net18, failures, name_str, path)
+
+    if not failures:
+        print("List is empty, very good")
+    else:
+        print(f"List is not empty: {failures}")
+        with open(os.path.join(path, "failures.txt"), "w", encoding="utf-8") as f:
+            for failure in failures:
+                f.write(failure + "\n")
+        raise UserWarning(f"Failures exist: {failures}")
+
+def evaluation_18_bus_gird(path: str = ".") -> None:
+
+
+    failures = pd.read_csv(
+        os.path.join(path, "failures.txt"),
+        header=None,
+        names=["algorithm", "iteration", "status"],
+        skipinitialspace=True,
+        dtype={"algorithm": str, "iteration": str, "status": str}
+    )
+    failures["iteration"] = failures["iteration"].astype(int)
+
+    # af_wls = estimate(net_af_wls, algorithm="af-wls", wlav=False)  # , af_target_value=.4, af_std_value=.15
+    # if not af_wls["success"]:
+    #     failures.append("AF-WLS estimation failed")
+    #     v_af_wls = np.full_like(net_af_wls.res_bus.vm_pu.values, np.nan, dtype=float)
+    #     delta_af_wls = np.full_like(net_af_wls.res_bus.va_degree.values, np.nan, dtype=float)
+    # else:
+    #     v_af_wls = net_af_wls.res_bus_est.vm_pu.values
+    #     delta_af_wls = net_af_wls.res_bus_est.va_degree.values
+    #
+    # # LAV
+    # af_lav = estimate(net_af_lav, algorithm="af-lp", wlav=False, with_ortools=False)
+    # if not af_lav["success"]:
+    #     failures.append("LAV estimation failed")
+    #     v_af_lav = np.full_like(net_af_lav.res_bus.vm_pu.values, np.nan, dtype=float)
+    #     delta_af_lav = np.full_like(net_af_lav.res_bus.va_degree.values, np.nan, dtype=float)
+    # else:
+    #     v_af_lav = net_af_lav.res_bus_est.vm_pu.values
+    #     delta_af_lav = net_af_lav.res_bus_est.va_degree.values
+    #
+    # # AF-WLAV
+    # af_wlav = estimate(net_af_wlav, algorithm="af-lp", wlav=True, with_ortools=False)
+    # if not af_wlav["success"]:
+    #     failures.append("AF-WLAV estimation failed")
+    #     v_af_wlav = np.full_like(net_af_wlav.res_bus.vm_pu.values, np.nan, dtype=float)
+    #     delta_af_wlav = np.full_like(net_af_wlav.res_bus.va_degree.values, np.nan, dtype=float)
+    # else:
+    #     v_af_wlav = net_af_wlav.res_bus_est.vm_pu.values
+    #     delta_af_wlav = net_af_wlav.res_bus_est.va_degree.values
     # 5. power flow results (runpp) aus net_base
-    v_pf = net_base.res_bus.vm_pu.values
-    delta_pf = net_base.res_bus.va_degree.values
-
-    # 6. Differences Estimation - PowerFlow
-    d_v_af_wls = v_af_wls - v_pf
-    d_a_af_wls = delta_af_wls - delta_pf
-
-    d_v_af_lav = v_af_lav - v_pf
-    d_a_af_lav = delta_af_lav - delta_pf
-
-    d_v_af_wlav = v_af_wlav - v_pf
-    d_a_af_wlav = delta_af_wlav - delta_pf
-
-    # 7. pack results into DataFrame
-    res_total_df = pd.DataFrame(
-        {
-            "V PF": v_pf,
-            "angle PF": delta_pf,
-            "V AF-WLS": v_af_wls,
-            "angle AF-WLS": delta_af_wls,
-            "V AF-LAV": v_af_lav,
-            "angle AF-LAV": delta_af_lav,
-            "V AF-WLAV": v_af_wlav,
-            "angle AF-WLAV": delta_af_wlav,
-        },
-        index=net_base.res_bus.index,  # Bus-Index als Index
-    )
-    res_total_df.to_excel(os.path.join(save_path, "total_df.xlsx"), index=False)
-
-    res_diff_df = pd.DataFrame(
-        {
-            "dV AF-WLS": d_v_af_wls,
-            "dA AF-WLS": d_a_af_wls,
-            "dV AF-LAV": d_v_af_lav,
-            "dA AF-LAV": d_a_af_lav,
-            "dV AF-WLAV": d_v_af_wlav,
-            "dA AF-WLAV": d_a_af_wlav,
-        },
-        index=net_base.res_bus.index,  # Bus-Index als Index
-    )
-    res_diff_df.to_excel(os.path.join(save_path, "diff_df.xlsx"), index=False)
-
-    res_diff_max_df = pd.DataFrame(
-        data={
-            "AF WLS V": [np.max(np.abs(d_v_af_wls))],
-            "AF WLS A": [np.max(np.abs(d_a_af_wls))],
-            "AF VLA V": [np.max(np.abs(d_v_af_lav))],
-            "AF VLA A": [np.max(np.abs(d_a_af_lav))],
-            "AF WLAV V": [np.max(np.abs(d_v_af_wlav))],
-            "AF WLAV A": [np.max(np.abs(d_a_af_wlav))]
-        }
-    )
-    res_diff_max_df.to_excel(os.path.join(save_path, "diff_max_df.xlsx"), index=False)
-
-    # set index for resolution dataframe (allocation factors)
-    af_wls["allocation_factors"].index = ["AF-WLS"]
-    af_wlav["allocation_factors"].index = ["AF-WLAV"]
-    af_lav["allocation_factors"].index = ["AF-LAV"]
-
-    res_af_df = pd.concat([af_wls["allocation_factors"], af_wlav["allocation_factors"],af_lav["allocation_factors"]])
-    res_af_df.to_excel(os.path.join(save_path, "af_df.xlsx"), index=True)
-
-    print(f"ende")
+    # v_pf = net_base.res_bus.vm_pu.values
+    # delta_pf = net_base.res_bus.va_degree.values
+    #
+    # # 6. Differences Estimation - PowerFlow
+    # d_v_af_wls = v_af_wls - v_pf
+    # d_a_af_wls = delta_af_wls - delta_pf
+    #
+    # d_v_af_lav = v_af_lav - v_pf
+    # d_a_af_lav = delta_af_lav - delta_pf
+    #
+    # d_v_af_wlav = v_af_wlav - v_pf
+    # d_a_af_wlav = delta_af_wlav - delta_pf
+    #
+    # # 7. pack results into DataFrame
+    # res_total_df = pd.DataFrame(
+    #     {
+    #         "V PF": v_pf,
+    #         "angle PF": delta_pf,
+    #         "V AF-WLS": v_af_wls,
+    #         "angle AF-WLS": delta_af_wls,
+    #         "V AF-LAV": v_af_lav,
+    #         "angle AF-LAV": delta_af_lav,
+    #         "V AF-WLAV": v_af_wlav,
+    #         "angle AF-WLAV": delta_af_wlav,
+    #     },
+    #     index=net_base.res_bus.index,  # Bus-Index als Index
+    # )
+    # res_total_df.to_excel(os.path.join(save_path, "total_df.xlsx"), index=False)
+    #
+    # res_diff_df = pd.DataFrame(
+    #     {
+    #         "dV AF-WLS": d_v_af_wls,
+    #         "dA AF-WLS": d_a_af_wls,
+    #         "dV AF-LAV": d_v_af_lav,
+    #         "dA AF-LAV": d_a_af_lav,
+    #         "dV AF-WLAV": d_v_af_wlav,
+    #         "dA AF-WLAV": d_a_af_wlav,
+    #     },
+    #     index=net_base.res_bus.index,  # Bus-Index als Index
+    # )
+    # res_diff_df.to_excel(os.path.join(save_path, "diff_df.xlsx"), index=False)
+    #
+    # res_diff_max_df = pd.DataFrame(
+    #     data={
+    #         "AF WLS V": [np.max(np.abs(d_v_af_wls))],
+    #         "AF WLS A": [np.max(np.abs(d_a_af_wls))],
+    #         "AF VLA V": [np.max(np.abs(d_v_af_lav))],
+    #         "AF VLA A": [np.max(np.abs(d_a_af_lav))],
+    #         "AF WLAV V": [np.max(np.abs(d_v_af_wlav))],
+    #         "AF WLAV A": [np.max(np.abs(d_a_af_wlav))]
+    #     }
+    # )
+    # res_diff_max_df.to_excel(os.path.join(save_path, "diff_max_df.xlsx"), index=False)
+    #
+    # # set index for resolution dataframe (allocation factors)
+    # af_wls["allocation_factors"].index = ["AF-WLS"]
+    # af_wlav["allocation_factors"].index = ["AF-WLAV"]
+    # af_lav["allocation_factors"].index = ["AF-LAV"]
+    #
+    # res_af_df = pd.concat([af_wls["allocation_factors"], af_wlav["allocation_factors"], af_lav["allocation_factors"]])
+    # res_af_df.to_excel(os.path.join(save_path, "af_df.xlsx"), index=True)
+    print(f"evaluation_18_bus_gird")
 
 
 if __name__ == "__main__":
-
-    s_path = r"D:\forschungspunkte\state_estimation\data\18_bus"  # ToDo: has to remove after testing
 
     mv_b: bool = False
     ieee14_b: bool = False
@@ -676,8 +745,9 @@ if __name__ == "__main__":
         net30 = _add_measurements_af(pn.case30(), 5, 112, .0, .0, .0)
 
     if bus18:
-        net18, k = _create_18_bus_grid(seed=112)
-        _create_measurement_18_bus_grid(net=net18, seed=112, rv=0, rp=0, rq=0)
-        calc_different_se(net18, save_path=s_path)
+        s_path = r"D:\forschungspunkte\state_estimation\data\18_bus\data"  # ToDo: has to remove after testing
+        create_random_grid_random_estimation(s_path, 100)
+        evaluation_18_bus_gird(s_path)
+
 
     print(f"whats up")
